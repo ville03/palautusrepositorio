253f96e2b874c91d8062a050b74e84d539f933d5
ville03 <ville.r.p.parkkinen@gmail.com>
2024-11-10 18:59:50 +0200

    teht 12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,46 @@
+"""moi"""
+import requests
 from player import Player
 
 def main():
-    pass
+    """moi"""
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-if __name__ == "__main__":
-    main()
+    for player in players:
+        print(player)
+
+class PlayerReader():
+    """moi"""
+    def __init__(self,url):
+        self.url=url
+
+    def get_players(self):
+        """moi"""
+        lista = requests.get(self.url, timeout=60).json()
+        players=[]
+        for player_dict in lista:
+            player = Player(player_dict)
+            players.append(player)
+        return players
+
+class PlayerStats():
+    """moi"""
+    def __init__(self,reader):
+        self.reader=reader
+
+    def top_scorers_by_nationality(self,nationality):
+        """moi"""
+        lista=[]
+        players=self.reader.get_players()
+        for i in players:
+            if i.country==nationality:
+                lista.append(i)
+        return sorted(lista,key=lambda t:t.total,reverse=True)
+
+        
+
+
+main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,13 @@
+"""moi"""
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-    
+    """moi"""
+    def __init__(self, dictionary):
+        self.name = dictionary['name']
+        self.country = dictionary['nationality']
+        self.team = dictionary['team']
+        self.goals = dictionary['goals']
+        self.assists = dictionary['assists']
+        self.total=self.goals+self.assists
+
     def __str__(self):
-        return self.name
+        return f"{self.name:20} {self.team} {self.goals} + {self.assists} = {self.total}"
