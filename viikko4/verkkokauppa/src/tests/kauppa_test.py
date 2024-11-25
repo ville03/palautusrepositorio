6326e3be0044840b4dcd322686f451b689225f29
ville03 <ville.r.p.parkkinen@gmail.com>
2024-11-25 22:03:51 +0200

    a

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -117,7 +117,7 @@ class TestKauppa(unittest.TestCase):
     def test_poista_korista(self):
         self.kauppa.aloita_asiointi()
         self.kauppa.lisaa_koriin(1)
-        self.kauppa.poista_korista(1) 
+        self.kauppa.poista_korista(1)  
         self.kauppa.lisaa_koriin(1)
         self.kauppa.tilimaksu("pekka", "12345")
 
